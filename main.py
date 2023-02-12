23b9a411e6ceca69fb7fa81f25f9e3807a78099d
Ravil <raviltchusmakaev@yandex.ru>
2023-02-12 20:36:26 +0300

    last commit_v1

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import json
 from pprint import pprint
 import requests
-
-def super_heroes():
-    url = "https://akabab.github.io/superhero-api/api/all.json"
-    response = requests.get(url=url)
-    return response
+import json
 
 def heroes():
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    response = requests.get(url=url)
     super_hero = []
-    for item in super_heroes():
-        intelligence = item.json()
+    for item in response.json():
+        intelligence = item
         try:
             for power_stats in intelligence['results']:
                 super_hero.append({
@@ -19,14 +16,12 @@ def heroes():
                 })
         except KeyError:
             print(f"Надо проверить ссылку")
-
     intelligence_super_man = 0
     name = ''
     for intelligence_super in super_hero:
         if intelligence_super_man < int(intelligence_super['intelligence']):
             intelligence_super_man = int(intelligence_super['intelligence'])
             name = intelligence_super['name']
-
     print(f"Самый умный {name}, интелект: {intelligence_super_man}")
+heroes()
 
-pprint(heroes())
